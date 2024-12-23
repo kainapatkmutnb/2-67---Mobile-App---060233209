180fb9da654ac0a961793fa27935f9a9b4e6b434
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2024-12-23 15:02:30 +0700

    create foodcourt app

---
+++ foodcourt/android/app/src/main/kotlin/com/example/foodcourt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foodcourt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ foodcourt/lib/foodMenu.dart
@@ -0,0 +1,7 @@
+class foodmenu {
+  final String name;
+  final String price;
+  final String img;
+
+  foodmenu(this.name, this.price, this.img);
+}
