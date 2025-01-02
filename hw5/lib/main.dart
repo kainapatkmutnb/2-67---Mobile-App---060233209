94bd80ea087207f4fd54c14b9deb6d612e1b6d46
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-01-02 17:23:12 +0700

    `Updated hw5/lib/main.dart and hw5/pubspec.yaml files with changes to UI and dependencies.`

--- hw5/lib/main.dart
+++ hw5/lib/main.dart
@@ -13,10 +13,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 56, 158, 226),
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'List Example'),
     );
   }
 }
@@ -30,14 +32,18 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Data {
+  late int id;
+  late String name;
+  late DateTime t;
+  Data(this.id, this.name, this.t);
+}
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  String txt = 'N/A';
+  List<Data> mylist = <Data>[];
+  int img = 0;
+  var list = ['one', 'two', 'three', 'four'];
 
   @override
   Widget build(BuildContext context) {
@@ -46,24 +52,77 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: img,
+                  onChanged: (int? value) {
+                    setState(() {
+                      img = 1;
+                    });
+                  },
+                ),
+                const CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage('assets/images/ig.png'),
+                ),
+              ],
+            ),
+            const TextField(),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  txt = 'Add item Success';
+                  mylist.add(Data(img, '1', DateTime.now()));
+                });
+              },
+              child: const Text('Add Item'),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              txt,
+              textScaleFactor: 2,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 550,
+              child: ListView.builder(
+                itemCount: mylist.length,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: double.infinity,
+                    height: 80,
+                    child: Card(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      color: Colors.primaries[index % Colors.primaries.length],
+                      child: ListTile(
+                        leading: const CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage('assets/images/icon2.png'),
+                        ),
+                        title: Text('Title Text (${mylist[index].id})'),
+                        subtitle: Text(mylist[index].t.toString()),
+                        trailing: const Icon(Icons.delete_rounded),
+                        onTap: () {
+                          setState(() {
+                            txt = 'Title Text ($index) is removed';
+                            mylist.removeAt(index);
+                          });
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
