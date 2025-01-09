bc315297c12e090be5188f5a3c70c12233615865
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-01-09 21:11:05 +0700

    `Added form fields for user information and buttons for adding and canceling data`

--- buttomnavi/lib/create_form/cl_create.dart
+++ buttomnavi/lib/create_form/cl_create.dart
@@ -8,6 +8,11 @@ class ClCreate extends StatefulWidget {
 }
 
 class ClCreateState extends State<ClCreate> {
+  var yourName = TextEditingController();
+  var surName = TextEditingController();
+  var yourUniversity = TextEditingController();
+  var yourFaculty = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,61 +20,96 @@ class ClCreateState extends State<ClCreate> {
         title: const Text('Create Form'),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
         child: Form(
           child: SingleChildScrollView(
             child: Column(
               children: [
                 pictureIcon(),
                 textheader('General Information'),
-                textForm('Your Name', 'Please insert your name'),
-                textForm('Sur Name', 'Please insert your name'),
+                textForm('Your Name', 'Please insert your name', yourName),
+                textForm('Sur Name', 'Please insert your name', surName),
                 textheader('Education Information'),
-                textForm('Your University', 'Please insert your University'),
-                textForm('Faculty', 'Please insert your Faculty'),
-                SizedBox(
-                  height: 10,
-                ),
+                textForm('Your University', 'Please insert your University', yourUniversity),
+                textForm('Faculty', 'Please insert your Faculty', yourFaculty),
+                const SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    SizedBox(width: 150, height: 50,
-                    child: ElevatedButton(onPressed: () {}, child: Text('Add Data')),)
+                    SizedBox(
+                      width: 150,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('Your Name: ' + yourName.text);
+                          print('Sur Name: ' + surName.text);
+                          print('Your University: ' + yourUniversity.text);
+                          print('Your Faculty: ' + yourFaculty.text);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent,
+                        ),
+                        child: const Text(
+                          'Add Data',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 150,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.redAccent,
+                          ),
+                          child: const Text(
+                            'Cancel',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 )
               ],
             ),
-          )),
+          ),
+        ),
       ),
     );
   }
 }
 
-TextFormField textForm(String ltext, String htext) {
+TextFormField textForm(String ltext, String htext, TextEditingController controller) {
   return TextFormField(
-                  decoration: InputDecoration(
-                    labelText: ltext,
-                    hintText: htext,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                );
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: ltext,
+      hintText: htext,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+    ),
+  );
 }
 
 Text textheader(String header) {
   return Text(
-                  header,
-                  style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueAccent),
-                );
+    header,
+    style: const TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+      color: Colors.blueAccent,
+    ),
+  );
 }
 
 Padding pictureIcon() {
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
+  return const Padding(
+    padding: EdgeInsets.all(8.0),
     child: Icon(
       Icons.insert_photo,
       size: 100,
