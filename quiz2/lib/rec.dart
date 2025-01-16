f9ba7077b43e95444c7d9bf43a4d0ea782df8357
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-01-16 14:52:38 +0700

    Add Rec widget to calculate and display rectangle area

---
+++ quiz2/lib/rec.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Rec extends StatelessWidget {
+  final double height;
+  final double width;
+
+  Rec({required this.height, required this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    double area = height * width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rectangle Area'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Go back to home screen
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Height: $height'),
+            Text('Width: $width'),
+            SizedBox(height: 20),
+            Text('Area: $area'),
+          ],
+        ),
+      ),
+    );
+  }
+}
