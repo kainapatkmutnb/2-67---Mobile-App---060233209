0c031bceae22133cc87627ee9850b00fd349595b
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-01-16 15:00:32 +0700

    `Added buttons for calculating rectangle, triangle, and circle areas`

--- quiz2/lib/home.dart
+++ quiz2/lib/home.dart
@@ -83,7 +83,40 @@ class _HomeState extends State<Home> {
                       );
                     }
                   },
-                  child: Text('Calculate Area'),
+                  child: Text('Calculate Rectangle Area'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState?.validate() ?? false) {
+                      // Navigate to tri.dart and pass base & height
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TriangleAreaPage(
+                            base: double.parse(baseController.text),
+                            height: double.parse(heightController.text),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Calculate Triangle Area'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState?.validate() ?? false) {
+                      // Navigate to cir.dart and pass radius
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CircleAreaPage(
+                            radius: double.parse(radiusController.text),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Calculate Circle Area'),
                 ),
               ],
             ],
