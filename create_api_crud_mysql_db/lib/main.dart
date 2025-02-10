b5321b3d2236de55145d9c563c7242e980bed010
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-02-10 21:34:58 +0700

    Refactor main.dart to use SingleChildScrollView for improved scrolling in the main layout

--- create_api_crud_mysql_db/lib/main.dart
+++ create_api_crud_mysql_db/lib/main.dart
@@ -47,15 +47,17 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('You have pushed the button this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
