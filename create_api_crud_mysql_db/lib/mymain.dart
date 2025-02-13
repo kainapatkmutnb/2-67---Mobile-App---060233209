ce585ee57ab7b9a83fffdf1f979be07ac305207e
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-02-13 10:03:53 +0700

    `Refactored code: updated color scheme, restored MyHomePage, and moved delData function`

--- create_api_crud_mysql_db/lib/mymain.dart
+++ create_api_crud_mysql_db/lib/mymain.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class ShowInf extends StatefulWidget {
-  const ShowInf({super.key});
+  const ShowInf({Key? key}) : super(key: key);
 
   @override
   State<ShowInf> createState() => _ShowInfState();
@@ -78,8 +78,7 @@ class _ShowInfState extends State<ShowInf> {
                         },
                       ),
                       IconButton(
-                        icon:
-                            const Icon(Icons.delete_outline, color: Colors.red),
+                        icon: const Icon(Icons.delete_outline, color: Colors.red),
                         onPressed: () => _showDel(list[index]["id"]),
                       ),
                     ],
@@ -183,19 +182,6 @@ class _ShowInfState extends State<ShowInf> {
     );
   }
 
-  void delData(int id) async {
-    var response = await http.delete(
-      Uri.http('10.4.13.41:8880', 'delete/$id'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-        "Accept": "application/json"
-      },
-    );
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-    listData();
-  }
-
   void addData() async {
     Map data = {
       'name': _nameController.text,
@@ -214,6 +200,19 @@ class _ShowInfState extends State<ShowInf> {
       body: body,
     );
 
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    listData();
+  }
+
+  void delData(int id) async {
+    var response = await http.delete(
+      Uri.http('10.4.13.41:8880', 'delete/$id'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        "Accept": "application/json"
+      },
+    );
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
     listData();
