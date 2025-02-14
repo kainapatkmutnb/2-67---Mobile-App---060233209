0f5e5593eee1e8f6ad347447acefe84599841bc2
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-02-14 15:42:50 +0700

    Add height, weight, BMI, and BMI type fields to Model class

--- cal_bmi_insert_to_db_with_api/lib/model.dart
+++ cal_bmi_insert_to_db_with_api/lib/model.dart
@@ -3,12 +3,20 @@ class Model {
   final String name;
   final String address;
   final String email;
+  final String height;
+  final String weight;
+  final String bmi;
+  final String bmiType;
 
   Model({
     required this.id,
     required this.name,
     required this.address,
     required this.email,
+    required this.height,
+    required this.weight,
+    required this.bmi,
+    required this.bmiType,
   });
 
   factory Model.fromJson(Map<String, dynamic> json) {
@@ -17,6 +25,10 @@ class Model {
       name: json['name'],
       address: json['address'],
       email: json['email'],
+      height: json['height'],
+      weight: json['weight'],
+      bmi: json['bmi'],
+      bmiType: json['bmiType'],
     );
   }
 }
