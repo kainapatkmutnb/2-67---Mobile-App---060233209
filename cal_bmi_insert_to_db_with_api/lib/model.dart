3929f0be802cfcaa523b7ac2b88f8115f02b5e31
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-02-14 16:28:22 +0700

    Rename Model class to Employee and update fields to use appropriate data types

--- cal_bmi_insert_to_db_with_api/lib/model.dart
+++ cal_bmi_insert_to_db_with_api/lib/model.dart
@@ -1,33 +1,33 @@
-class Model {
-  final String id;
+class Employee {
+  final int id;
   final String name;
-  final String address;
   final String email;
   final String phone;
-  final String height;
-  final String weight;
-  final String bmi;
+  final String address;
+  final double height;
+  final double weight;
+  final double bmi;
   final String bmiType;
 
-  Model({
+  Employee({
     required this.id,
     required this.name,
-    required this.address,
     required this.email,
     required this.phone,
+    required this.address,
     required this.height,
     required this.weight,
     required this.bmi,
     required this.bmiType,
   });
 
-  factory Model.fromJson(Map<String, dynamic> json) {
-    return Model(
+  factory Employee.fromJson(Map<String, dynamic> json) {
+    return Employee(
       id: json['id'],
       name: json['name'],
-      address: json['address'],
       email: json['email'],
       phone: json['phone'],
+      address: json['address'],
       height: json['height'],
       weight: json['weight'],
       bmi: json['bmi'],
