2538b778373c0a8f87eddafa66bb46c7c436e137
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-02-20 09:20:48 +0700

    Remove hardcoded NDK version in build.gradle.kts to use Flutter's NDK version

--- webview/android/app/build.gradle.kts
+++ webview/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.webview"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
