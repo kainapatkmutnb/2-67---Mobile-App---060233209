dea5103fd035ca4dcbb1fb57d8eabd3cf5929134
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-03-05 18:17:16 +0700

    Add collision response for Ball with Bat and implement removal delay

--- brick_breaker/lib/src/components/ball.dart
+++ brick_breaker/lib/src/components/ball.dart
@@ -1,8 +1,10 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flutter/material.dart';
 
 import '../brick_breaker.dart';
+import 'bat.dart';
 import 'play_area.dart';
 
 class Ball extends CircleComponent with CollisionCallbacks, HasGameReference<BrickBreaker> {
@@ -38,8 +40,13 @@ class Ball extends CircleComponent with CollisionCallbacks, HasGameReference<Bri
       } else if (intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       } else if (intersectionPoints.first.y >= game.height) {
-        removeFromParent();
+        add(RemoveEffect(
+          delay: 0.35,
+        ));
       }
+    } else if (other is Bat) {
+      velocity.y = -velocity.y;
+      velocity.x = velocity.x + (position.x - other.position.x) / other.size.x * game.width * 0.3;
     } else {
       debugPrint('collision with $other');
     }
